e836f1156a1dd7726a5540cd8b05714e301e78e4
ikolesov <ikolesov@inttrust.ru>
2025-02-03 14:12:54 +0300

    tests

--- tests/test_horo_generator.py
+++ tests/test_horo_generator.py
@@ -2,9 +2,10 @@ from app.services.horo_generator import generate_horoscope_text
 
 def test_generate_horoscope_text(mock_openai):
     sign = "leo"
+    positions = calculate_planetary_positions(date)
     aspects = [("sun", "moon", 120)]  # Пример аспектов
     houses = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]  # Пример домов
-    prediction = generate_horoscope_text(sign, aspects, houses)
+    prediction = generate_horoscope_text(sign, positions, aspects, houses)
     assert isinstance(prediction, str)
     assert len(prediction) > 0
 
